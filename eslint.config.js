40996e6ad558ce651b52a059a95e1fb748f44f55
Dave <lapojapod@gmail.com>
2024-08-22 10:32:01 +0800

    Missing valid types checking (Working on it <3)

--- eslint.config.js
+++ eslint.config.js
@@ -7,14 +7,21 @@ export default [
     languageOptions: { globals: globals.browser }
   },
   pluginJs.configs.recommended,
-  jsdoc.configs['flat/recommended-error'],
+  jsdoc.configs['flat/recommended-typescript-flavor-error'],
   {
     files: ['**/*.js'],
     plugins: {
       jsdoc,
     },
     rules: {
-      'jsdoc/require-description': 'error'
+      'jsdoc/require-description': 'warn',
+      'jsdoc/check-param-names' : 'error',
+      'jsdoc/check-types': 'error',
+      'jsdoc/require-param-name': 'error',
+      'jsdoc/require-returns': 'error',
+      'jsdoc/require-returns-type': 'error',
+      'jsdoc/require-jsdoc': 'error',
+      'jsdoc/valid-types': 'error',
     }
   },
 ];
--- main.js
+++ main.js
@@ -2,14 +2,22 @@ import express from 'express';
 
 const app = express();
 
-const krazy = 2;
-
-const krazy2 = krazy + 1;
-
 const object = {
     name: 'krazy',
     value: false,
 }
+
+/**
+ * This is a function... a krazy function
+ * @param {string} name - The name of the function
+ * @param {number} value - The value of the function
+ * @returns {void}
+ */
+function krazyFunction(name, value) {
+    console.log(name, value);
+}
+
+krazyFunction(object.value, object.name);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
